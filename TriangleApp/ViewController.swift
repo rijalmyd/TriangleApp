f16975a0e019add7ab7000fdcd68f101e677c81c
rijalmyd <muhyidinrijal@gmail.com>
2025-11-20 16:04:30 +0700

    Initial Commit

---
+++ TriangleApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TriangleApp
+//
+//  Created by Rijal Muhyidin on 20/11/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
